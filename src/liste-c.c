fd2217452075b0e4f58f5e967631886a02489183
Miahy <miahy.raharijaona@esiroi.re>
2026-04-10 08:16:00 +0000

    Ajustement en vu d'erreur

--- src/liste-c.c
+++ src/liste-c.c
@@ -1,1 +1,15 @@
 #include "liste-c.h"
+#include <stdlib.h> 
+
+/**
+ * Structure interne d'un noeud de la liste
+ */
+struct st_nodeLC {
+    int value;                // La donnée stockée (entier)
+    struct st_nodeLC *next;   // Pointeur vers le noeud suivant
+};
+
+/**
+ * Alias de type pour manipuler des pointeurs de noeuds plus facilement
+ */
+typedef struct st_nodeLC * NodeLC;
